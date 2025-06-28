1ca467f9777cb19bbeed5885fde71eb9e037230e
Liliana Sanfilippo <lsanfilippo@techfak.uni-bielefeld.de>
2025-06-28 13:03:52 +0200

    test html removed: browserify
    Hilft nicht

--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import * as path from 'path';
 import webpack, { Configuration } from 'webpack';
 import { LicenseWebpackPlugin } from 'license-webpack-plugin';
 const LicensePlugin: any = LicenseWebpackPlugin;
@@ -9,6 +9,26 @@ const require = createRequire(import.meta.url);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+console.log('Checking imports and variables...');
+
+try {
+    console.log('path:', typeof path !== 'undefined');
+    console.log('webpack:', typeof webpack !== 'undefined');
+    console.log('LicenseWebpackPlugin:', typeof LicenseWebpackPlugin !== 'undefined');
+    console.log('require:', typeof require !== 'undefined');
+    console.log('__dirname:', __dirname);
+    console.log('__filename:', __filename);
+} catch (e) {
+    console.error('Import/require problem:', e);
+    process.exit(1); // Falls du in node arbeitest
+}
+
+console.log("VOR CONFIG")
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('LIB_TYPE:', process.env.LIB_TYPE);
+
 
 console.log('Using ProvidePlugin to inject process and Buffer');
 
@@ -71,12 +91,16 @@ const config: Configuration = {
         extensions: ['.ts', '.js'],
         fallback: {
             fs: false,
-            path: require.resolve('path-browserify'),
             assert: require.resolve('assert/'),
             buffer: require.resolve('buffer/'),
             process: require.resolve('process/browser'),
         },
     },
 };
+
+console.log("NACH CONFIG")
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('LIB_TYPE:', process.env.LIB_TYPE);
+console.log('Webpack build target:', config.target);
 
 export default config;
